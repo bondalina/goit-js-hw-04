28bc517ffb1e9bc2807b41b5f9aca882f511ec74
bondalina <142501520+bondalina@users.noreply.github.com>
2023-11-15 17:44:43 +0200

    update task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,9 @@
 function calcAverageCalories(days) {
+    if (days.length === 0) {
+        return 0;
+    }
 
+    
     let totalCalories = 0;
     for (const day of days) {
         totalCalories += day.calories;
